4dc65203953f30fc04aa404c622e294cc56e75c5
Aaron Fortner <afortner@islander.tamucc.edu>
2021-11-16 23:48:56 -0600

    CRUDs work, need to neaten up HTML and some forms but otherwise does job admin

--- JAM mkII/Controllers/JobListController.cs
+++ JAM mkII/Controllers/JobListController.cs
@@ -14,8 +14,20 @@ namespace JAM_mkII.Controllers
         // GET
         public IActionResult Index()
         {
-            var jobs = Context.Jobs.OrderBy(j => j.StoreId).ToList();
+            var jobs = Context.Jobs.OrderBy(j => j.JobId).ToList();
             return View(jobs);
+        }
+
+        public IActionResult JobAdmin()
+        {
+            var jobs = Context.Jobs.OrderBy(j => j.JobId).ToList();
+            return View(jobs);
+        }
+
+        public IActionResult Add()
+        {
+            ViewBag.Action = "Add Job";
+            return View("Edit", new Job());
         }
 
         [HttpGet]
@@ -42,5 +54,58 @@ namespace JAM_mkII.Controllers
                 return View("CreateJob", subj);
             }
         }
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            ViewBag.Action = "Edit Job";
+            var job = Context.Jobs.Find(id);
+            return View(job);
+        }
+
+
+        [HttpPost]
+        public IActionResult Edit(Job job)
+        {
+            if (ModelState.IsValid)
+            {
+                if (job.JobId == 0)
+                {
+                    Context.Jobs.Add(job);
+                }
+                else
+                {
+                    Context.Jobs.Update(job);
+                }
+
+                Context.SaveChanges();
+                return RedirectToAction("JobAdmin");
+            }
+            else
+            {
+                ViewBag.Action = (job.JobId == 0) ? "Add" : "Edit";
+                return View(job);
+            }
+
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            var job = Context.Jobs.Find(id);
+            return View(job);
+        }
+
+        [HttpPost]
+        public IActionResult Delete(Job job)
+        {
+            Context.Jobs.Remove(job);
+            Context.SaveChanges();
+            return RedirectToAction("JobAdmin");
+        }
+
+
+
+
     }
 }
