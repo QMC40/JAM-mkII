8aecaaecb1c51b1c4ea09f54bbc1174822f7e6f3
Aaron Fortner <afortner@islander.tamucc.edu>
2021-11-16 23:56:15 -0600

    added Admin button to nav bar for testing

--- JAM mkII/Controllers/JobListController.cs
+++ JAM mkII/Controllers/JobListController.cs
@@ -30,30 +30,30 @@ namespace JAM_mkII.Controllers
             return View("Edit", new Job());
         }
 
-        [HttpGet]
-        public IActionResult CreateJob()
-        {
-            ViewBag.Positions = Context.Positions.OrderBy(p => p.PositionId).ToList();
-            ViewBag.Stores = Context.Stores.OrderBy(s => s.StoreId).ToList();
-            return View(new Job());
-        }
-
-        [HttpPost]
-        public IActionResult CreateJob(Job subj)
-        {
-            if (ModelState.IsValid)
-            {
-                Context.Jobs.Add(subj);
-                Context.SaveChanges();
-                var jobs = Context.Jobs.OrderBy(j => j.StoreId).ToList();
-                return View("Index", jobs);
-            }
-            else
-            {
-                ViewBag.Action = "There's a problem with your information, please check and resubmit";
-                return View("CreateJob", subj);
-            }
-        }
+        // [HttpGet]
+        // public IActionResult CreateJob()
+        // {
+        //     ViewBag.Positions = Context.Positions.OrderBy(p => p.PositionId).ToList();
+        //     ViewBag.Stores = Context.Stores.OrderBy(s => s.StoreId).ToList();
+        //     return View(new Job());
+        // }
+        //
+        // [HttpPost]
+        // public IActionResult CreateJob(Job subj)
+        // {
+        //     if (ModelState.IsValid)
+        //     {
+        //         Context.Jobs.Add(subj);
+        //         Context.SaveChanges();
+        //         var jobs = Context.Jobs.OrderBy(j => j.StoreId).ToList();
+        //         return View("Index", jobs);
+        //     }
+        //     else
+        //     {
+        //         ViewBag.Action = "There's a problem with your information, please check and resubmit";
+        //         return View("CreateJob", subj);
+        //     }
+        // }
 
         [HttpGet]
         public IActionResult Edit(int id)
@@ -62,7 +62,6 @@ namespace JAM_mkII.Controllers
             var job = Context.Jobs.Find(id);
             return View(job);
         }
-
 
         [HttpPost]
         public IActionResult Edit(Job job)
