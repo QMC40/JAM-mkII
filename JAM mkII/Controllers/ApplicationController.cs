12f6bbb2326a201bbd684b115bc37fd16b805c6b
panzer <afortner@islander.tamucc.edu>
2021-11-17 15:43:33 -0600

    working on apply system

--- JAM mkII/Controllers/ApplicationController.cs
+++ JAM mkII/Controllers/ApplicationController.cs
@@ -1,13 +1,25 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using JAM_mkII.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace JAM_mkII.Controllers
 {
     public class ApplicationController : Controller
     {
+    private JobManagerContext Context { get; set; }
+
+        public ApplicationController(JobManagerContext ctx) => Context = ctx;
+  
         // GET
         public IActionResult Index()
         {
             return View();
+        }
+
+
+        public IActionResult Apply(int id)
+        {
+            var job = Context.Jobs.Find(id);
+            return View(job);
         }
     }
 }
---
+++ JAM mkII/Migrations/20211117161101_StartApplication.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace JAM_mkII.Migrations
+{
+    public partial class StartApplication : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "Disqualified",
+                table: "Applications",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "PassedTest",
+                table: "Applications",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Disqualified",
+                table: "Applications");
+
+            migrationBuilder.DropColumn(
+                name: "PassedTest",
+                table: "Applications");
+        }
+    }
+}
