7f33d81574e5a1d7ff1241c3a26ca8a1eed13aca
Aaron Fortner <afortner@islander.tamucc.edu>
2021-11-26 15:28:11 -0600

    fixed nav tab in admin section

--- JAM mkII/Models/ViewModels/Nav.cs
+++ JAM mkII/Models/ViewModels/Nav.cs
@@ -2,14 +2,10 @@
 {
     public static class Nav
     {
-        public static string Active(string value, string current)
-        {
-            return value == current ? "active" : "";
-        }
+        public static string Active(string value, string current) =>
+            (value == current) ? "active" : "";
 
-        public static string Active(int value, int current)
-        {
-            return value == current ? "active" : "";
-        }
+        public static string Active(int value, int current) =>
+            (value == current) ? "active" : "";
     }
 }
